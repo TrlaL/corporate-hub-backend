b90759d77adbc52a5b2656dc3d8cd596b5dbd88f
Simkin Mark <marks.simkin@gmail.com>
2025-03-27 10:58:58 +0300

    init

---
+++ server.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import { google } from 'googleapis'
+
+const SPREADSHEET_ID = '1qrYkjYTnDpkRTHSN4u3422jhq9g18qxE_sOCC9wwBaY'
+const SHEET_NAME = 'Лист1'
+
+const app = express()
+const port = 3000
+
+const auth = new google.auth.GoogleAuth({
+  keyFile: 'key.json',
+  scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'],
+})
+
+app.get('/api/sheet', async (req, res) => {
+  try {
+    const sheets = google.sheets({ version: 'v4', auth })
+    const response = await sheets.spreadsheets.values.get({ spreadsheetId: SPREADSHEET_ID, range: SHEET_NAME })
+    res.json(response.data.values)
+  } catch (error) {
+    res.status(500)
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Сервер запущен на http://localhost:${port}`);
+})
